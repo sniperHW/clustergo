1000fe437bb7f61a5b31993c7801862c735ef1ea
sniperHW <sniperHW@163.com>
2023-09-09 22:12:40 +0800

    none

--- rpc.go
+++ rpc.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"sync"
 	"time"
 	"unsafe"
 
@@ -21,6 +22,8 @@ type rpcChannel struct {
 	peer addr.LogicAddr
 	node *node
 	self *Node
+	name string
+	once sync.Once
 }
 
 func (c *rpcChannel) SendRequest(request *rpcgo.RequestMsg, deadline time.Time) error {
@@ -36,7 +39,16 @@ func (c *rpcChannel) Reply(response *rpcgo.ResponseMsg) error {
 }
 
 func (c *rpcChannel) Name() string {
-	return fmt.Sprintf("%s <-> %s", c.self.localAddr.LogicAddr(), c.peer.String())
+	c.once.Do(func() {
+		selfAddr := c.self.localAddr.LogicAddr().String()
+		peerAddr := c.peer.String()
+		if selfAddr > peerAddr {
+			c.name = fmt.Sprintf("%s <-> %s", selfAddr, peerAddr)
+		} else {
+			c.name = fmt.Sprintf("%s <-> %s", peerAddr, selfAddr)
+		}
+	})
+	return c.name
 }
 
 func (c *rpcChannel) Identity() uint64 {
@@ -50,6 +62,8 @@ func (c *rpcChannel) Peer() addr.LogicAddr {
 // 自连接channel
 type selfChannel struct {
 	self *Node
+	name string
+	once sync.Once
 }
 
 func (c *selfChannel) SendRequest(request *rpcgo.RequestMsg, deadline time.Time) error {
@@ -74,7 +88,11 @@ func (c *selfChannel) Reply(response *rpcgo.ResponseMsg) error {
 }
 
 func (c *selfChannel) Name() string {
-	return fmt.Sprintf("%s <-> %s", c.self.localAddr.LogicAddr(), c.self.localAddr.LogicAddr())
+	c.once.Do(func() {
+		localAddrStr := c.self.localAddr.LogicAddr().String()
+		c.name = fmt.Sprintf("%s <-> %s", localAddrStr, localAddrStr)
+	})
+	return c.name
 }
 
 func (c *selfChannel) Identity() uint64 {
